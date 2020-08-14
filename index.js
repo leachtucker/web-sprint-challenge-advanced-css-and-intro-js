3d21b10e794d3a78d498b264d0270c68fdac3517
Tucker Leach <leachtucker@gmail.com>
2020-08-14 09:49:13 -0700

    Finished task 5 of JS

--- index.js
+++ index.js
@@ -259,11 +259,12 @@ console.log(get20s(artists));
  * 
  * Note that sucessfully invoking this function multiple times without refreshing your browser will continuously remove artists from the array until there are none left. If you refresh your browser, the data will reset.  
  */
-function removeArtist( /*code here*/ ) {
-    /* code here */
+function removeArtist(artistArr, index) {
+    artistArr.splice(index, 1);
+    console.log(artistArr.length);
 }
 
-
+removeArtist(artists, 0);
 
 /**
 /* Task 6: Create a function called `addArtist` that can accept an object of information and add it to the artists array. Then, add a 21st artist to the array (you) with custom information!👩‍🎨👨‍🎨
